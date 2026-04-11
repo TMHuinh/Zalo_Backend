d0ed2e0e09dcfb0ced2b94ae26932838b73cebcb
nguyenvu512 <vu5122004@gmail.com>
2026-04-11 11:49:57 +0700

    [NguyenVu] Add api get conversation by user id

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,5 @@ router.use("/auth", require("../modules/auth/auth.routes"));
 router.use("/user", require("../modules/user/user.routes"));
 router.use("/friendship", require("../modules/friendship/friendship.routes"));
 router.use("/message", require("../modules/message/message.routes"));
+router.use("/conversation", require("../modules/conversation/conversation.route"));
 module.exports = router;
