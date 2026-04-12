0abf99a85f77c238f443c8b5b328890ba7ab6233
duy1606 <duyzzzzz405@gmail.com>
2026-04-12 09:31:35 +0700

    update-router

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,12 @@ router.use("/auth", require("../modules/auth/auth.routes"));
 router.use("/user", require("../modules/user/user.routes"));
 router.use("/friendship", require("../modules/friendship/friendship.routes"));
 router.use("/message", require("../modules/message/message.routes"));
-router.use("/conversation", require("../modules/conversation/conversation.route"));
+router.use(
+  "/notification",
+  require("../modules/notification/notification.routes"),
+);
+router.use(
+  "/conversation",
+  require("../modules/conversation/conversation.route"),
+);
 module.exports = router;
